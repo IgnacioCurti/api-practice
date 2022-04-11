781a7a4f31ff324ba90223d81985075e85470e43
IgnacioCurti <ignaciocurtijristo@gmail.com>
2022-04-11 17:03:29 -0300

    arregle el error

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
-var evolutionChainLimit = 476
+var evolutionChainLimit = 476 - 1
 
 var emptySpaces = 6
 
 async function getPokemon(memberNumber){
-    const id = Math.floor(Math.random() * evolutionChainLimit);
+    const id = Math.floor(Math.random() * evolutionChainLimit) + 1;
     const responseChain = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
     const evolutionaryChain = await responseChain.json();
     const urlPokemon = getFinalEvolution(evolutionaryChain.chain);
-    console.log(urlPokemon);
-    //const responseSpecies = await fetch(urlPokemon);
-    //const pokemonSpecies = await responseSpecies.json();
+    const responseSpecies = await fetch(urlPokemon);
+    const pokemonSpecies = await responseSpecies.json();
     //console.log(pokemonSpecies);
-    //const pokemonName = pokemonSpecies.name;
-    const responsePokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${urlPokemon}`);
+    const pokemonNumber = pokemonSpecies.id;
+    const responsePokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`);
     const pokemon = await responsePokemon.json();
     document.getElementById(`${memberNumber}mon`).src = pokemon.sprites.front_default;  
 }
 
-
-
-function getFinalEvolution(object, evoNumber = -1) {
+function getFinalEvolution(object, evoNumber = -1){
     if (evoNumber >= 0){
-        if (object[evoNumber].evolves_to.length > 0){
-            const randomEvoNumber = Math.floor(Math.random() * object[evoNumber].evolves_to.length)
-            return (getFinalEvolution(object[evoNumber].evolves_to, randomEvoNumber))
-        } else{
-            return (object[evoNumber].species.name)
-        }
+        return condition(object[evoNumber])
     } else{
-        if (object.evolves_to.length > 0){
-            const randomEvoNumber = Math.floor(Math.random() * object.evolves_to.length)
-            return (getFinalEvolution(object.evolves_to, randomEvoNumber))
-        } else {return object.species.name}
+        return condition(object)
+    }    
+}
+
+function condition(object){
+    if (object.evolves_to.length > 0){
+        const randomEvoNumber = Math.floor(Math.random() * object.evolves_to.length)
+        return (getFinalEvolution(object.evolves_to, randomEvoNumber))
+    } else{
+        return (object.species.url)
     }
 }
 
@@ -41,6 +39,7 @@ function getTeam(){
         getPokemon(memberNumber)
         }
 }
+
 
 function clearImg(){
 }
