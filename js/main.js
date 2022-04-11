7b6ac833fe95aa6f23639624ac0084e44b1c4950
IgnacioCurti <ignaciocurtijristo@gmail.com>
2022-04-11 11:44:25 -0300

    corrected some things

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ var evolutionChainLimit = 476
 var emptySpaces = 6
 
 async function getPokemon(memberNumber){
-    const id = 99//Math.floor(Math.random() * evolutionChainLimit);
+    const id = 33//Math.floor(Math.random() * evolutionChainLimit);
     const responseChain = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
     const evolutionaryChain = await responseChain.json();
-    const urlPokemon = getFinalEvolution(evolutionaryChain.chain)
-    console.log(urlPokemon)
-    const responsePokemon = await fetch(urlPokemon);
-    const pokemon = responsePokemon.json();
+    const urlPokemon = getFinalEvolution(evolutionaryChain.chain);
+    console.log(urlPokemon);
+    const responseSpecies = await fetch(urlPokemon);
+    const pokemonSpecies = await responseSpecies.json();
+    console.log(pokemonSpecies);
+    const pokemonName = pokemonSpecies.name;
+    const responsePokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemon = await responsePokemon.json();
     document.getElementById(`${memberNumber}mon`).src = pokemon.sprites.front_default;  
 }
 
@@ -24,15 +28,21 @@ function getFinalEvolution(object) {
     }
     if (arrayEvolvesTo.length > 0){
         const randomEvoNumber = Math.floor(Math.random() * object.evolves_to.length)
+        console.log("increiblementellegué acá 1")
+
         getFinalEvolution(object.evolves_to[randomEvoNumber - 1])
-    } else {findSpeciesByEvolvesTo(object, isArrayFlag)}
+        console.log("increiblementellegué acá 2")
+
+    } else {return findSpeciesByEvolvesTo(object, isArrayFlag)}
 }
 
 
 function findSpeciesByEvolvesTo(object, isArrayFlag) {
+    console.log("increiblementellegué acá 3")
     if (isArrayFlag){
+        console.log("increiblementellegué acá 4")
 
-    } else{return object.species.name}
+    } else{return object.species.url}
 }
 
 function getTeam(){
